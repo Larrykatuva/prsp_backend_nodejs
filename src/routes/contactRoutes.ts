6640fe0dd6f0b76a4299bc3c50620d975e3dd091
lkatuva <lkatuva@jambopa.com>
2022-11-10 22:36:41 +0300

    Added token authentication guard

--- src/routes/contactRoutes.ts
+++ src/routes/contactRoutes.ts
@@ -1,26 +1,32 @@
 import { Router } from 'express';
 import express from 'express';
 import ContactsController from "../controllers/contactsController";
+import {AuthGuard} from "../guards";
+import authGuard from "../guards/authGuard";
 
 const router: Router = express.Router();
 
 router.post(
     '/upload-csv',
+    authGuard,
     ContactsController.uploadCsv
 )
 
 router.get(
     '/groups',
+    authGuard,
     ContactsController.listContactGroups
 )
 
 router.get(
     '/group/:id',
+    authGuard,
     ContactsController.getContactGroupById
 )
 
 router.get(
     '/group/:id/contacts',
+    authGuard,
     ContactsController.getGroupContacts
 )
 
